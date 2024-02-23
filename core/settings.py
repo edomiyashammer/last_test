d7fe130a3df9cc2e967640381a8088f410e91bf3
edomiyashammer <edomiashammer@gmail.com>
2024-02-23 11:47:19 +0300

    first commit

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ STATICFILES_STORAGE = (
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 JAZZMIN_SETTINGS = {
     "site_header": "Giyaa Marketplace P.L.C",
